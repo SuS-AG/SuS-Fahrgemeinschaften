5942994f1260c12fcedbfa4c52a59b130d32914c
Patrik <lktpriv@gmail.com>
2023-02-14 12:50:39 +0100

    added logo, welcome-message & input fields

---
+++ src/pages/register.tsx
@@ -0,0 +1,34 @@
+import logo from "../../public/assets/logo.svg";
+import Image from "next/image";
+import { Input } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+
+export default function Register() {
+  return (
+    <div className="container h-[100%] w-[100%]">
+      <div
+        className="container mx-auto 
+      flex h-[100%] w-[100%]"
+      >
+        <Image
+          src={logo}
+          alt=""
+          className="mx-auto my-[79px] h-[128px] w-[128px]"
+        />
+      </div>
+      <div className="container  justify-center text-center">
+        <p className="text-4xl font-extrabold"> Willkommen</p>
+        <p className="text-2xl font-extrabold">bei</p>
+        <p className="text-2xl font-extrabold">SUS-Fahrgemeinschaften</p>
+      </div>
+      <div className="container m-5 mx-auto w-56 justify-center">
+        <Input placeholder="E-Mail" className="my-2" />
+        <Input placeholder="Passwort" className="my-2" />
+        <Input placeholder="Passwort wiederholen" className="my-2" />
+        <Button colorScheme="teal" size="md" > Anmelden</Button>
+
+      </div>
+
+    </div>
+  );
+}
